e2ada4094e139f60963d67b66a96a63c742c43a6
mardiansyah17 <mardiansyahm002@gmail.com>
2023-08-11 10:54:48 +0700

    update

--- database/migrations/2023_08_07_023643_create_prices_table.php
+++ database/migrations/2023_08_07_023643_create_prices_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -16,7 +17,7 @@ return new class extends Migration {
             $table->id();
             $table->timestamps();
             $table->integer("price");
-            $table->foreignId("venue_id")->constrained('venues');
+            $table->foreignId("venue_id")->constrained('venues')->cascadeOnDelete();
         });
     }
 
